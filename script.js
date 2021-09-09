2155a2f4173b7f1fa0155cdd7dd0f052dcc90b89
nadellasaideep1 <64152721+nadellasaideep1@users.noreply.github.com>
2021-09-09 17:03:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,235 @@
+const tagsEl = document.getElementById('tags');
+const textarea = document.getElementById('textarea');
+
+// focus by default
+textarea.focus();
+
+textarea.addEventListener('keyup', (e) => {
+	// create a tag for all the inputs separated by a comma
+	createTags(e.target.value);
+	
+	// check if the enter key is pressed
+	if(e.key === 'Enter') {
+		// empty textarea
+		// used setTimeout to add a little delay in order to clean the input
+		setTimeout(() => {
+			e.target.value = '';
+		}, 10)
+		
+		// start randomizer
+		randomSelect();
+	}
+	
+});
+
+function createTags(input) {
+	const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim());
+	
+	// clean up the tags first
+	tagsEl.innerHTML = '';
+	
+	// map over the tags and add them to the tagsEl container
+	tags.forEach(tag => {
+		const tagEl = document.createElement('span');
+		tagEl.classList.add('tag');
+		tagEl.innerText = tag;
+		tagsEl.appendChild(tagEl);
+	})
+}
+
+function randomSelect() {
+	const times = 30;
+	
+	const interval = setInterval(() => {
+		const randomTag = pickRandomTag();
+		
+		highlightTag(randomTag);
+		
+		// remove the highlight after a while
+		setTimeout(() => {
+			unhighlightTag(randomTag);
+		}, 100);
+	}, 100);
+	
+	// allow times * 100 ms for the tags to randomly "highlight" themselves
+	// then pick another tag
+	setTimeout(() => {
+		clearInterval(interval);
+		
+		setTimeout(() => {
+			const randomTag = pickRandomTag();
+			
+			highlightTag(randomTag)
+		}, 100);
+	}, times * 100);
+	const confetti = document.getElementById('confetti');
+const confettiCtx = confetti.getContext('2d');
+let container, confettiElements = [], clickPosition;
+
+// helper
+rand = (min, max) => Math.random() * (max - min) + min;
+
+// params to play with
+const confettiParams = {
+    // number of confetti per "explosion"
+    number: 70,
+    // min and max size for each rectangle
+    size: { x: [5, 20], y: [10, 18] },
+    // power of explosion
+    initSpeed: 25,
+    // defines how fast particles go down after blast-off
+    gravity: 0.65,
+    // how wide is explosion
+    drag: 0.08,
+    // how slow particles are falling
+    terminalVelocity: 6,
+    // how fast particles are rotating around themselves
+    flipSpeed: 0.017,
+};
+const colors = [
+    { front : '#3B870A', back: '#235106' },
+    { front : '#B96300', back: '#6f3b00' },
+    { front : '#E23D34', back: '#88251f' },
+    { front : '#CD3168', back: '#7b1d3e' },
+    { front : '#664E8B', back: '#3d2f53' },
+    { front : '#394F78', back: '#222f48' },
+    { front : '#008A8A', back: '#005353' },
+];
+
+setupCanvas();
+updateConfetti();
+
+confetti.addEventListener('click', addConfetti);
+window.addEventListener('resize', () => {
+    setupCanvas();
+    hideConfetti();
+});
+
+// Confetti constructor
+function Conf() {
+    this.randomModifier = rand(-1, 1);
+    this.colorPair = colors[Math.floor(rand(0, colors.length))];
+    this.dimensions = {
+        x: rand(confettiParams.size.x[0], confettiParams.size.x[1]),
+        y: rand(confettiParams.size.y[0], confettiParams.size.y[1]),
+    };
+    this.position = {
+        x: clickPosition[0],
+        y: clickPosition[1]
+    };
+    this.rotation = rand(0, 2 * Math.PI);
+    this.scale = { x: 1, y: 1 };
+    this.velocity = {
+        x: rand(-confettiParams.initSpeed, confettiParams.initSpeed) * 0.4,
+        y: rand(-confettiParams.initSpeed, confettiParams.initSpeed)
+    };
+    this.flipSpeed = rand(0.2, 1.5) * confettiParams.flipSpeed;
+
+    if (this.position.y <= container.h) {
+        this.velocity.y = -Math.abs(this.velocity.y);
+    }
+
+    this.terminalVelocity = rand(1, 1.5) * confettiParams.terminalVelocity;
+
+    this.update = function () {
+        this.velocity.x *= 0.98;
+        this.position.x += this.velocity.x;
+
+        this.velocity.y += (this.randomModifier * confettiParams.drag);
+        this.velocity.y += confettiParams.gravity;
+        this.velocity.y = Math.min(this.velocity.y, this.terminalVelocity);
+        this.position.y += this.velocity.y;
+
+        this.scale.y = Math.cos((this.position.y + this.randomModifier) * this.flipSpeed);
+        this.color = this.scale.y > 0 ? this.colorPair.front : this.colorPair.back;
+    }
+}
+
+function updateConfetti () {
+    confettiCtx.clearRect(0, 0, container.w, container.h);
+
+    confettiElements.forEach((c) => {
+        c.update();
+        confettiCtx.translate(c.position.x, c.position.y);
+        confettiCtx.rotate(c.rotation);
+        const width = (c.dimensions.x * c.scale.x);
+        const height = (c.dimensions.y * c.scale.y);
+        confettiCtx.fillStyle = c.color;
+        confettiCtx.fillRect(-0.5 * width, -0.5 * height, width, height);
+        confettiCtx.setTransform(1, 0, 0, 1, 0, 0)
+    });
+
+    confettiElements.forEach((c, idx) => {
+        if (c.position.y > container.h ||
+            c.position.x < -0.5 * container.x ||
+            c.position.x > 1.5 * container.x) {
+            confettiElements.splice(idx, 1)
+        }
+    });
+    window.requestAnimationFrame(updateConfetti);
+}
+
+function setupCanvas() {
+    container = {
+        w: confetti.clientWidth,
+        h: confetti.clientHeight
+    };
+    confetti.width = container.w;
+    confetti.height = container.h;
+}
+
+function addConfetti(e) {
+    const canvasBox = confetti.getBoundingClientRect();
+    if (e) {
+        clickPosition = [
+            e.clientX - canvasBox.left,
+            e.clientY - canvasBox.top
+        ];
+    } else {
+        clickPosition = [
+            canvasBox.width * Math.random(),
+            canvasBox.height * Math.random()
+        ];
+    }
+    for (let i = 0; i < confettiParams.number; i++) {
+        confettiElements.push(new Conf())
+    }
+}
+
+function hideConfetti() {
+    confettiElements = [];
+    window.cancelAnimationFrame(updateConfetti)
+}
+
+confettiLoop();
+function confettiLoop() {
+    addConfetti();
+    setTimeout(confettiLoop, 700 + Math.random() * 1700);
+}
+}
+
+function pickRandomTag() {
+	const tags = document.querySelectorAll('.tag');
+	return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+	tag.classList.add('highlight');
+}
+
+function unhighlightTag(tag) {
+	tag.classList.remove('highlight');
+}
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+	social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+	social_panel_container.classList.remove('visible')
+});
